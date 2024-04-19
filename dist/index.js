cfe7826ab4657861f8bcb5c5114199af69d401db
charle <charlexavier100@gmail.com>
2024-04-19 19:33:23 -0300

    organizando header

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,4 @@
 "use strict";
-// import { Client } from "pg";
-// import dotenv from "dotenv";
-// import * as bcrypt from "bcrypt";
-// import { UsersService } from "./services/usersServices";
-// import { verifyToken } from "./conections/authorization";
-// import express from "express";
-// export const SECRET = process.env.SECRET;
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     var desc = Object.getOwnPropertyDescriptor(m, k);
@@ -43,126 +36,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.jwt = exports.db = exports.SECRET = void 0;
-// import cors from "cors"; // Importing cors as ES module
-// dotenv.config();
-// const app = express();
-// const port = 3000;
-// app.use(express.json());
-// app.use(cors());
-// export const db = new Client({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASS,
-//   port: 5433,
-// });
-// db.connect();
-// const usersService = new UsersService(db);
-// export const jwt = require("jsonwebtoken"); // You may leave this line as is if using CommonJS require syntax
-// const bodyParser = require("body-parser"); // You may leave this line as is if using CommonJS require syntax
-// app.post("/login", async (req, res, next) => {
-//   try {
-//     const { usuario, senha } = req.body;
-//     const foundUser = await usersService.getByUsername(usuario);
-//     if (foundUser) {
-//       const correctPassword = await bcrypt.compare(senha, foundUser.senha);
-//       if (correctPassword) {
-//         const token = jwt.sign({ usuario: foundUser.usuario }, SECRET, {
-//           expiresIn: 300, // expires in 5 minutes
-//         });
-//         return res.json({ auth: true, token: token });
-//       }
-//     }
-//     res.status(401).json({ auth: false, message: "Credenciais inválidas." });
-//   } catch (error) {
-//     res.status(500).json({ error: "Erro ao tentar fazer login." });
-//   }
-// });
-// app.get("/usuarios", verifyToken, async (req, res) => {
-//   try {
-//     const usuarios = await usersService.getAll();
-//     res.json(usuarios);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-// app.get("/usuarios/:id", verifyToken, async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const user = await usersService.find(id);
-//     res.json(user);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-// app.post("/usuarios", async (req, res) => {
-//   try {
-//     const user = await usersService.create(req.body);
-//     res.json(user);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-// app.put("/usuarios/:id", verifyToken, async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const userDataToUpdate = req.body;
-//     const existingUser = await usersService.find(id);
-//     if (!existingUser) {
-//       return res.status(404).json({ message: "Usuário não encontrado" });
-//     }
-//     const updatedUser = await usersService.update(id, userDataToUpdate);
-//     res.json({ message: "Usuário atualizado com sucesso", user: updatedUser });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-// app.delete("/usuarios/:id", verifyToken, async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     await usersService.delete(id);
-//     res.json({ message: "Usuário excluído com sucesso" });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-// app.post("/check-email", async (req, res) => {
-//   try {
-//     const { email } = req.body;
-//     const user = await usersService.getByEmail(email);
-//     if (user) {
-//       res.json({ message: "Um link de recuperação foi enviado para o seu email.. Sucesso!" });
-//     } else {
-//       res.status(200).json({ message: "Email não encontrado" });
-//     }
-//   } catch (error) {
-//     console.error("Erro na verificação de email:", error);
-//     res.status(500).json({ error: "Erro interno do servidor" });
-//   }
-// });
-// app.listen(port, () => {
-//   console.log("server run", port);
-// });
 const pg_1 = require("pg");
 const dotenv_1 = __importDefault(require("dotenv"));
 const bcrypt = __importStar(require("bcrypt"));
 const usersServices_1 = require("./services/usersServices");
 const authorization_1 = require("./conections/authorization");
+exports.SECRET = process.env.SECRET;
 const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors")); // Importando cors como módulo ES
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = 3000;
+// Configuração do CORS
+const cors_1 = __importDefault(require("cors"));
+app.use((0, cors_1.default)({
+    origin: 'http://127.0.0.1:5501' // Permitindo apenas esta origem
+}));
 app.use(express_1.default.json());
-// Configurando o CORS com opções específicas
-const corsOptions = {
-    origin: "*", // Permitindo solicitações de qualquer origem. Você pode especificar origens específicas aqui.
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Permitindo os métodos HTTP especificados.
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-};
-app.use((0, cors_1.default)(corsOptions));
-exports.SECRET = process.env.SECRET;
 exports.db = new pg_1.Client({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
@@ -172,7 +61,7 @@ exports.db = new pg_1.Client({
 });
 exports.db.connect();
 const usersService = new usersServices_1.UsersService(exports.db);
-exports.jwt = require("jsonwebtoken"); // Você pode deixar esta linha como está se estiver usando a sintaxe require comum
+exports.jwt = require("jsonwebtoken");
 app.post("/login", (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { usuario, senha } = req.body;
@@ -180,7 +69,7 @@ app.post("/login", (req, res, next) => __awaiter(void 0, void 0, void 0, functio
         if (foundUser) {
             const correctPassword = yield bcrypt.compare(senha, foundUser.senha);
             if (correctPassword) {
-                const token = exports.jwt.sign({ usuario: foundUser.usuario }, exports.SECRET, {
+                const token = exports.jwt.sign({ usuario: foundUser.usuario }, process.env.SECRET, {
                     expiresIn: 300, // expires in 5 minutes
                 });
                 return res.json({ auth: true, token: token });
@@ -262,5 +151,5 @@ app.post("/check-email", (req, res) => __awaiter(void 0, void 0, void 0, functio
     }
 }));
 app.listen(port, () => {
-    console.log("server run", port);
+    console.log("Servidor rodando na porta", port);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,156 +1,22 @@
-// import { Client } from "pg";
-// import dotenv from "dotenv";
-// import * as bcrypt from "bcrypt";
-// import { UsersService } from "./services/usersServices";
-// import { verifyToken } from "./conections/authorization";
-// import express from "express";
-// export const SECRET = process.env.SECRET;
-
-// import cors from "cors"; // Importing cors as ES module
-
-// dotenv.config();
-// const app = express();
-// const port = 3000;
-
-// app.use(express.json());
-// app.use(cors());
-
-// export const db = new Client({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASS,
-//   port: 5433,
-// });
-
-// db.connect();
-
-// const usersService = new UsersService(db);
-
-// export const jwt = require("jsonwebtoken"); // You may leave this line as is if using CommonJS require syntax
-
-// const bodyParser = require("body-parser"); // You may leave this line as is if using CommonJS require syntax
-
-// app.post("/login", async (req, res, next) => {
-//   try {
-//     const { usuario, senha } = req.body;
-//     const foundUser = await usersService.getByUsername(usuario);
-
-//     if (foundUser) {
-//       const correctPassword = await bcrypt.compare(senha, foundUser.senha);
-
-//       if (correctPassword) {
-//         const token = jwt.sign({ usuario: foundUser.usuario }, SECRET, {
-//           expiresIn: 300, // expires in 5 minutes
-//         });
-//         return res.json({ auth: true, token: token });
-//       }
-//     }
-
-//     res.status(401).json({ auth: false, message: "Credenciais inválidas." });
-//   } catch (error) {
-//     res.status(500).json({ error: "Erro ao tentar fazer login." });
-//   }
-// });
-// app.get("/usuarios", verifyToken, async (req, res) => {
-//   try {
-//     const usuarios = await usersService.getAll();
-//     res.json(usuarios);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-
-// app.get("/usuarios/:id", verifyToken, async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const user = await usersService.find(id);
-//     res.json(user);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-
-// app.post("/usuarios", async (req, res) => {
-//   try {
-//     const user = await usersService.create(req.body);
-//     res.json(user);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-// app.put("/usuarios/:id", verifyToken, async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const userDataToUpdate = req.body;
-
-//     const existingUser = await usersService.find(id);
-
-//     if (!existingUser) {
-//       return res.status(404).json({ message: "Usuário não encontrado" });
-//     }
-
-//     const updatedUser = await usersService.update(id, userDataToUpdate);
-
-//     res.json({ message: "Usuário atualizado com sucesso", user: updatedUser });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-
-// app.delete("/usuarios/:id", verifyToken, async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     await usersService.delete(id);
-//     res.json({ message: "Usuário excluído com sucesso" });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
-
-// app.post("/check-email", async (req, res) => {
-//   try {
-//     const { email } = req.body;
-//     const user = await usersService.getByEmail(email);
-
-//     if (user) {
-//       res.json({ message: "Um link de recuperação foi enviado para o seu email.. Sucesso!" });
-//     } else {
-//       res.status(200).json({ message: "Email não encontrado" });
-//     }
-//   } catch (error) {
-//     console.error("Erro na verificação de email:", error);
-//     res.status(500).json({ error: "Erro interno do servidor" });
-//   }
-// });
-
-// app.listen(port, () => {
-//   console.log("server run", port);
-// });
 import { Client } from "pg";
 import dotenv from "dotenv";
 import * as bcrypt from "bcrypt";
 import { UsersService } from "./services/usersServices";
 import { verifyToken } from "./conections/authorization";
+export const SECRET = process.env.SECRET
 import express from "express";
-import cors from "cors"; // Importando cors como módulo ES
 
 dotenv.config();
 const app = express();
 const port = 3000;
 
+// Configuração do CORS
+import cors from 'cors';
+app.use(cors({
+  origin: 'http://127.0.0.1:5501' // Permitindo apenas esta origem
+}));
+
 app.use(express.json());
-
-// Configurando o CORS com opções específicas
-const corsOptions = {
-  origin: "*", // Permitindo solicitações de qualquer origem. Você pode especificar origens específicas aqui.
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Permitindo os métodos HTTP especificados.
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-};
-app.use(cors(corsOptions));
-
-export const SECRET = process.env.SECRET;
 
 export const db = new Client({
   user: process.env.DB_USER,
@@ -164,102 +30,103 @@ db.connect();
 
 const usersService = new UsersService(db);
 
-export const jwt = require("jsonwebtoken"); // Você pode deixar esta linha como está se estiver usando a sintaxe require comum
+export const jwt = require("jsonwebtoken");
 
 app.post("/login", async (req, res, next) => {
-    try {
-      const { usuario, senha } = req.body;
-      const foundUser = await usersService.getByUsername(usuario);
-  
-      if (foundUser) {
-        const correctPassword = await bcrypt.compare(senha, foundUser.senha);
-  
-        if (correctPassword) {
-          const token = jwt.sign({ usuario: foundUser.usuario }, SECRET, {
-            expiresIn: 300, // expires in 5 minutes
-          });
-          return res.json({ auth: true, token: token });
-        }
+  try {
+    const { usuario, senha } = req.body;
+    const foundUser = await usersService.getByUsername(usuario);
+
+    if (foundUser) {
+      const correctPassword = await bcrypt.compare(senha, foundUser.senha);
+
+      if (correctPassword) {
+        const token = jwt.sign({ usuario: foundUser.usuario }, process.env.SECRET, {
+          expiresIn: 300, // expires in 5 minutes
+        });
+        return res.json({ auth: true, token: token });
       }
-  
-      res.status(401).json({ auth: false, message: "Credenciais inválidas." });
-    } catch (error) {
-      res.status(500).json({ error: "Erro ao tentar fazer login." });
     }
-  });
-  app.get("/usuarios", verifyToken, async (req, res) => {
-    try {
-      const usuarios = await usersService.getAll();
-      res.json(usuarios);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+
+    res.status(401).json({ auth: false, message: "Credenciais inválidas." });
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao tentar fazer login." });
+  }
+});
+
+app.get("/usuarios", verifyToken, async (req, res) => {
+  try {
+    const usuarios = await usersService.getAll();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/usuarios/:id", verifyToken, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await usersService.find(id);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/usuarios", async (req, res) => {
+  try {
+    const user = await usersService.create(req.body);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.put("/usuarios/:id", verifyToken, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userDataToUpdate = req.body;
+
+    const existingUser = await usersService.find(id);
+
+    if (!existingUser) {
+      return res.status(404).json({ message: "Usuário não encontrado" });
     }
-  });
-  
-  app.get("/usuarios/:id", verifyToken, async (req, res) => {
-    try {
-      const { id } = req.params;
-      const user = await usersService.find(id);
-      res.json(user);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+
+    const updatedUser = await usersService.update(id, userDataToUpdate);
+
+    res.json({ message: "Usuário atualizado com sucesso", user: updatedUser });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.delete("/usuarios/:id", verifyToken, async (req, res) => {
+  try {
+    const { id } = req.params;
+    await usersService.delete(id);
+    res.json({ message: "Usuário excluído com sucesso" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/check-email", async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await usersService.getByEmail(email);
+
+    if (user) {
+      res.json({ message: "Um link de recuperação foi enviado para o seu email.. Sucesso!" });
+    } else {
+      res.status(200).json({ message: "Email não encontrado" });
     }
-  });
-  
-  app.post("/usuarios", async (req, res) => {
-    try {
-      const user = await usersService.create(req.body);
-      res.json(user);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
-  app.put("/usuarios/:id", verifyToken, async (req, res) => {
-    try {
-      const { id } = req.params;
-      const userDataToUpdate = req.body;
-  
-      const existingUser = await usersService.find(id);
-  
-      if (!existingUser) {
-        return res.status(404).json({ message: "Usuário não encontrado" });
-      }
-  
-      const updatedUser = await usersService.update(id, userDataToUpdate);
-  
-      res.json({ message: "Usuário atualizado com sucesso", user: updatedUser });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
-  
-  app.delete("/usuarios/:id", verifyToken, async (req, res) => {
-    try {
-      const { id } = req.params;
-      await usersService.delete(id);
-      res.json({ message: "Usuário excluído com sucesso" });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
-  
-  app.post("/check-email", async (req, res) => {
-    try {
-      const { email } = req.body;
-      const user = await usersService.getByEmail(email);
-  
-      if (user) {
-        res.json({ message: "Um link de recuperação foi enviado para o seu email.. Sucesso!" });
-      } else {
-        res.status(200).json({ message: "Email não encontrado" });
-      }
-    } catch (error) {
-      console.error("Erro na verificação de email:", error);
-      res.status(500).json({ error: "Erro interno do servidor" });
-    }
-  });
-  
+  } catch (error) {
+    console.error("Erro na verificação de email:", error);
+    res.status(500).json({ error: "Erro interno do servidor" });
+  }
+});
 
 app.listen(port, () => {
-  console.log("server run", port);
+  console.log("Servidor rodando na porta", port);
 });
